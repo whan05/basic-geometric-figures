bb4d67860fe0914a6fa8eda57272f5b6f0e25218
Whanderley Fonseca <wfonsecap-m@ucenfotec.ac.cr>
2022-06-06 15:26:10 -0600

    We add some files with some variables and console.logs to check the variables

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # basic-geometric-figures
 This is an basic geometric figures app
+
+
+# Laboratory #1 : Geometrics Figures
+
+- Step 1: Define the shape of the figure
+- Step 2: Implement the algorithm in the JavaScript file
+- Step 3: Create functions
+- Step 4: Intgrate HTML with JavaScript
---
+++ figures.js
@@ -0,0 +1,47 @@
+// This is the sqare code
+
+console. group(' Squares')
+
+const squareSides = 5;
+
+console.log(`side of the square is ${squareSides}cm`);
+
+const squarePerimeter = squareSides * 4;
+
+console.log(`The perimeter square is ${squarePerimeter}cm`);
+
+const areaSquare = squareSides * squareSides;
+
+console.log(`The area square is ${areaSquare}cm^2`);
+
+console. groupEnd()
+
+// This is the triangle code
+
+console. group(' Triangles')
+
+const triangleSides1 = 6;
+const triangleSides2 = 6;
+const triangleBase = 4;
+const triangleHeight = 5.5;
+
+console.log(`The sides of the triangle are: ${triangleSides1} cm, ${triangleSides2} cm, ${triangleBase} cm`);
+
+console.log(' The height of the triangle is: ${triangleHeight}');
+
+const trianglePerimeter = triangleSides1 + triangleSides2 + triangleBase;
+
+console.log(`The perimeter Triangle is ${trianglePerimeter}cm`);
+
+const areaTrangle = (triangleBase * triangleHeight) /2;
+
+console.log(`The area triangle is ${areaTrangle}cm^2`);
+
+console. groupEnd()
+
+// This is the circle code
+
+console. group(' Circle ')
+
+
+console. groupEnd()
